5448fbae13e9ae0809771b7c5a25015cc5a0dd3a
Adalto Picotti Junior <adaltopicotti.jr@gmail.com>
2021-10-20 16:36:36 -0300

    first commit

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -17,7 +17,7 @@ type Message = {
 
 const messagesQueue: Message[] = []
 
-const socket = io('http://localhost:4000');
+const socket = io(import.meta.env.VITE_API_URL);
 
 socket.on('new_message', newMessage => {
   messagesQueue.push(newMessage)
